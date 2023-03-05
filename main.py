18f75eb64f0a8beabb960eb98ee56059c4adf2e2
Trupti Kondhare <97141718+TruptiKondhare@users.noreply.github.com>
2023-03-06 00:55:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,210 @@
+'''a=20.3
+B=type(a)
+print(B)'''
+import math
+
+'''A="           Hello"
+result=A.strip()
+print(result)'''
+
+'''S="I live an apple ,i eat daily"
+result=S.count("a")
+print(result)'''
+
+
+'''S="HEllo"
+result=S.startswith("H")
+print(result)'''
+
+
+'''S="Hello"
+result=S.startswith("h")
+print(result)'''
+
+
+'''S="Hello"
+result=S.endswith("o")
+print(result)'''
+
+
+
+'''S="hi Pcharm"
+result=S.swapcase()
+print(result)'''
+
+'''A="Alexa the great"
+result=A.index("the")
+print(result)'''
+
+
+
+'''A="a,f,f,f,d,d"
+result=A.split()
+print(result)'''
+
+
+'''a=['f','s','f']
+b=['df','qs','fq']
+result =a.extend(b)
+print(result)'''
+
+'''li=[2,3,4,6,8,9]
+result=li.pop()
+print(result)'''
+'''
+li =[2,2,4,5,7,8]
+result=li.reverse()
+print(result)'''
+
+
+
+'''S=[2,2,2,2,23,3,3,3,3356,678]
+result=sum(S)
+print(result)'''
+
+
+'''s=[2,4,8,9,0,2,4,5,7,9,9,6,5,4,33,6,7,8]
+result=s[0]+s[1]+s[2]+s[3]+s[4]+s[5]+s[6]+s[7]
+print(result)'''
+
+
+'''a =20
+a =a+10
+print(a)'''
+
+
+'''a=[839,376,354,9,0,3]
+a= 0 in a
+print(a)'''
+
+
+'''
+a=math.pi
+print(a)'''
+
+'''a=math.e
+print(a)'''
+
+
+'''a =math.factorial(8)
+print(a)'''
+
+
+'''name=input("Enter your name:-")
+if name=="siri":
+    print("hi siri gm")
+else:
+    print("gm User")'''
+
+
+
+'''a =56
+b=67
+if a==50:
+    print("Hi")
+elif b==60:
+    print("hello")
+else:
+    print("bye")'''
+
+'''brand =input("Enter your name:-")
+if brand=="Honda":
+    print("Honda is indian brand")
+elif brand=="Bajaj":
+    print("Bajaj is indian brand")
+elif brand=="Hero":
+    print("Hero is indian")
+else:
+    print("ALl brands are good")'''
+
+
+
+'''print("hello world")'''
+
+
+'''a=10
+b=29
+c=20
+result=a+b+c
+print(result)'''
+
+
+
+'''x = input("Enter 1")
+y = input("Enter 2")
+print(x + y)'''
+
+'''x = input("Type a number: ")
+y = input("Type another number: ")
+sum = int(x) + int(y)
+print("The sum is: ", sum)'''
+
+
+# This program adds two numbers
+
+'''num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print(sum)'''
+
+'''a=input (" Enter your first no:-" )
+b=input("Enter second no:-")
+sum=int( a) + int( b)
+print(sum)'''
+
+#Program of prime no
+'''
+a=7
+if a/2==0:
+    print("prime no")
+else:
+    print("no is not prime no")'''
+
+'''
+int a=input("Enter a no")
+if a/2==0:
+    print("It is a prime no:")
+else:
+    print("it is not a prime no:")'''
+
+#Moduels
+'''import Mro
+Mro.mro()'''
+
+'''from Mro import mro
+mro()
+'''
+'''from Mro import mro3 as mo
+mo()'''
+
+'''import Mro
+Mro.login'''
+import a1.a2.a
+a1.a2.a.login
+
+from a1.a2.a import login
+object=login()
+print(object.username)
+print(object.passward)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
