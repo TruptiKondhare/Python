8cff147b4431dddf64ec52c7d9b530c1d479dd3d
Trupti Kondhare <97141718+TruptiKondhare@users.noreply.github.com>
2023-03-08 00:04:34 +0530

    Delete pandas1.py

--- pandas1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-pf=pd.read_excel('practice_excel.xlsx',sheet_name='Sheet1')
-pf1=pd.read_excel('practice_excel.xlsx',sheet_name='Sheet2')
-pf2=pd.read_excel('practice_excel.xlsx',sheet_name='Sheet3')
-
-p=pd.merge(pf,pf1,how="inner",on="Client_Code")
-p1=pd.merge(p,pf2,how="inner",on='Client_Code')
-
-print(p1)
